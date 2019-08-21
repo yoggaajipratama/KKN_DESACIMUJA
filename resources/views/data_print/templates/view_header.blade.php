a3e532448395a0d8ad7914d93597881d91572bcc
yoggaajipratama <yoggaajipratama99@gmail.com>
2019-08-21 11:21:21 +0700

    Bootstrap

---
+++ resources/views/data_print/templates/view_header.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+	<div class="container">
+		<div class="container-fluid">
+			<div class="row align-items-center justify-content-center">
+				<img src="{{asset('images/Sumedang.png')}}" style="width: 210px;height: 150px">
+			</div>
+		</div>
+	</div>
+</body>
